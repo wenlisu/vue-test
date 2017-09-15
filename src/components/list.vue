5de8934d602f4d4ad55aea6d5eb18b94e42c5e14
wenlisu <415357008@qq.com>
2017-09-15 11:01:00 +0800

    add:首页

---
+++ src/components/list.vue
@@ -0,0 +1,21 @@
+<template>
+    <mt-cell :title="name"  is-link>
+        <img slot="icon" :src="img" width="24" height="24" >
+        <span style="color:#ccc;" v-if="data">{{ data }}元</span>
+    </mt-cell>
+</template>
+
+<script>
+    export default {
+        name: 'list',
+        data() {
+            return {
+             
+            }
+        },
+        props: ['name','img','data'],
+        components: {}
+    }
+</script>
+
+
--- src/views/personalCenter.vue
+++ src/views/personalCenter.vue
@@ -1,21 +1,50 @@
 <template>
-    <mt-header title="个人中心">
-        <router-link to="/" slot="left">
-            <mt-button icon="back"></mt-button>
-        </router-link>
-    </mt-header>
+    <div>
+        <mt-header title="个人中心">
+            <router-link to="/" slot="left">
+                <mt-button icon="back"></mt-button>
+            </router-link>
+        </mt-header>
+        <div class="headerImg flex justifyContentCenter alignItemsCenter">
+            <mt-spinner type="double-bounce"></mt-spinner>
+        </div>
+        <ul class="listDate flexDirectionRow">
+            <li class="flex1 justifyContentCenter alignItemsCenter">我的单车<span>0</span></li>
+            <li class="flex1 justifyContentCenter alignItemsCenter">我的售纸机<span>0</span></li>
+        </ul>
+        <list :name="title1" :img="imgUrl1" :data="data1"></list>
+        <list :name="title2" :img="imgUrl2" :data="data2"></list>
+        <list :name="title3" :img="imgUrl3" :data="data3"></list>
+        <list :name="title4" :img="imgUrl4" :data="data4"></list>
+    </div>
 </template>
 
 <script>
-// import headerItem from '@/components/header';
-export default {
-    name: 'personalCenter',
-    data() {
-        return {
+    import list from '@/components/list';
+    export default {
+        name: 'personalCenter',
+        data() {
+            return {
+                title1: '资产管理',
+                imgUrl1: '/static/images/personal-center-icon1.png',
+                data1: '0.00',
+                title2: '贝壳投资',
+                imgUrl2: '/static/images/personal-center-icon2.png',
+                data2: '',
+                title3: '关于我们',
+                imgUrl3: '/static/images/personal-center-icon3.png',
+                data3: '',
+                title4: '练习我们',
+                imgUrl4: '/static/images/personal-center-icon4.png',
+                data4: '',
+            }
+        },
+        components: {
+            list
         }
-    },
-    components: {
-        // headerItem
     }
-}
 </script>
+
+<style lang="scss" scoped>
+    @import '../style/personalCenter';
+</style>
